bedf8b9eea988551abed0ed07ce03ca24e2d2525
zhaoyunxing92 <2385585770@qq.com>
2020-04-04 23:43:52 +0800

    [init]

---
+++ src/main/java/io/github/sunny/cloud/file/CloudFileApplication.java
@@ -0,0 +1,13 @@
+package io.github.sunny.cloud.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFileApplication.class, args);
+    }
+
+}
