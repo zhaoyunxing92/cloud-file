3cfae2d9f917336ab38d695c5527800e3fa7e83d
zhaoyunxing92 <2385585770@qq.com>
2020-04-05 23:18:35 +0800

    [up]添加of方法

--- src/main/java/io/github/sunny/cloud/file/result/Response.java
+++ src/main/java/io/github/sunny/cloud/file/result/Response.java
@@ -3,9 +3,60 @@
  */
 package io.github.sunny.cloud.file.result;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author zhaoyunxing92
  * @date: 2020-04-05 17:43
  */
-public class Response {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response implements Serializable {
+    /**
+     * code编码
+     */
+    private Integer code;
+    /**
+     * 消息
+     */
+    private String msg;
+    /**
+     * 数据
+     */
+    private Object data;
+
+    public static Response of(Integer code, String msg, Object data) {
+        return new Response(code, msg, data);
+    }
+
+    /**
+     * 成功code=0
+     *
+     * @param msg  成功消息
+     * @param data 数据
+     * @return
+     */
+    public static Response of(String msg, Object data) {
+        return new Response(0, msg, data);
+    }
+
+    /**
+     * 成功code=0
+     *
+     * @param msg  成功消息
+     * @param data 数据
+     * @return
+     */
+    public static Response of(Object data) {
+        return new Response(0, "ok", data);
+    }
+
+    public static Response of(Integer code, String msg) {
+        return new Response(code, msg, null);
+    }
 }
